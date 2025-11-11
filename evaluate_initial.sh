fd79a593a85d0198635dfc0c1136844e89d49e2a
Nsivaa <alberto.dicembre@gmail.com>
2025-11-11 18:57:47 +0100

    added dash in between hyperparamters

--- evaluate_initial.sh
+++ evaluate_initial.sh
@@ -7,7 +7,7 @@ dropout_rate_list=(0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8)
 # iterate over all the values of the dropout rate list
 
 # iterate over all dropout rates
-for dropout_rate in "${dropout_rate_list[@]}"; do
+for dropout_rate in "${dropout_rate_list[@]} "; do
     echo "Running experiment with dropout_rate=${dropout_rate}"
 
     python src/evaluate_initial.py \
